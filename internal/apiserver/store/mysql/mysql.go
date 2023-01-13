0cd39d81743a2cbbbf00652b90ee8fc32316777f
skeleton1231 <814010079@qq.com>
2023-01-14 00:22:58 +0800

    feat:store|mysql

---
+++ internal/apiserver/store/mysql/mysql.go
@@ -0,0 +1,137 @@
+// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package mysql
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/marmotedu/errors"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/store"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/logger"
+	genericoptions "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/options"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/pkg/db"
+	"gorm.io/gorm"
+)
+
+type datastore struct {
+	db *gorm.DB
+
+	// can include two database instance if needed
+	// docker *grom.DB
+	// db *gorm.DB
+}
+
+/*func (ds *datastore) Users() store.UserStore {
+	return newUsers(ds)
+}
+
+func (ds *datastore) Secrets() store.SecretStore {
+	return newSecrets(ds)
+}
+
+func (ds *datastore) Policies() store.PolicyStore {
+	return newPolicies(ds)
+}
+
+func (ds *datastore) PolicyAudits() store.PolicyAuditStore {
+	return newPolicyAudits(ds)
+}*/
+
+func (ds *datastore) Close() error {
+	db, err := ds.db.DB()
+	if err != nil {
+		return errors.Wrap(err, "get gorm db instance failed")
+	}
+
+	return db.Close()
+}
+
+var (
+	mysqlFactory store.Factory
+	once         sync.Once
+)
+
+// GetMySQLFactoryOr create mysql factory with the given config.
+func GetMySQLFactoryOr(opts *genericoptions.MySQLOptions) (store.Factory, error) {
+	if opts == nil && mysqlFactory == nil {
+		return nil, fmt.Errorf("failed to get mysql store fatory")
+	}
+
+	var err error
+	var dbIns *gorm.DB
+	once.Do(func() {
+		options := &db.Options{
+			Host:                  opts.Host,
+			Username:              opts.Username,
+			Password:              opts.Password,
+			Database:              opts.Database,
+			MaxIdleConnections:    opts.MaxIdleConnections,
+			MaxOpenConnections:    opts.MaxOpenConnections,
+			MaxConnectionLifeTime: opts.MaxConnectionLifeTime,
+			LogLevel:              opts.LogLevel,
+			Logger:                logger.New(opts.LogLevel),
+		}
+		dbIns, err = db.New(options)
+
+		// uncomment the following line if you need auto migration the given models
+		// not suggested in production environment.
+		// migrateDatabase(dbIns)
+
+		mysqlFactory = &datastore{dbIns}
+	})
+
+	if mysqlFactory == nil || err != nil {
+		return nil, fmt.Errorf("failed to get mysql store fatory, mysqlFactory: %+v, error: %w", mysqlFactory, err)
+	}
+
+	return mysqlFactory, nil
+}
+
+// cleanDatabase tear downs the database tables.
+// nolint:unused // may be reused in the feature, or just show a migrate usage.
+func cleanDatabase(db *gorm.DB) error {
+	/*if err := db.Migrator().DropTable(&v1.User{}); err != nil {
+		return errors.Wrap(err, "drop user table failed")
+	}
+	if err := db.Migrator().DropTable(&v1.Policy{}); err != nil {
+		return errors.Wrap(err, "drop policy table failed")
+	}
+	if err := db.Migrator().DropTable(&v1.Secret{}); err != nil {
+		return errors.Wrap(err, "drop secret table failed")
+	}*/
+
+	return nil
+}
+
+// migrateDatabase run auto migration for given models, will only add missing fields,
+// won't delete/change current data.
+// nolint:unused // may be reused in the feature, or just show a migrate usage.
+func migrateDatabase(db *gorm.DB) error {
+	/*if err := db.AutoMigrate(&v1.User{}); err != nil {
+		return errors.Wrap(err, "migrate user model failed")
+	}
+	if err := db.AutoMigrate(&v1.Policy{}); err != nil {
+		return errors.Wrap(err, "migrate policy model failed")
+	}
+	if err := db.AutoMigrate(&v1.Secret{}); err != nil {
+		return errors.Wrap(err, "migrate secret model failed")
+	}*/
+
+	return nil
+}
+
+// resetDatabase resets the database tables.
+// nolint:unused,deadcode // may be reused in the feature, or just show a migrate usage.
+func resetDatabase(db *gorm.DB) error {
+	if err := cleanDatabase(db); err != nil {
+		return err
+	}
+	if err := migrateDatabase(db); err != nil {
+		return err
+	}
+
+	return nil
+}
