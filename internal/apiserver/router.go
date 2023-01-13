4748184bc70219f61694b2ccfc03aa3106f1d975
skeleton1231 <814010079@qq.com>
2023-01-14 00:10:01 +0800

    feat:router

---
+++ internal/apiserver/router.go
@@ -0,0 +1,15 @@
+package apiserver
+
+import "github.com/gin-gonic/gin"
+
+func initRouter(g *gin.Engine) {
+	installMiddleware(g)
+	installController(g)
+}
+
+func installMiddleware(g *gin.Engine) {
+}
+
+func installController(g *gin.Engine) *gin.Engine {
+	return g
+}
