86ea7da55dfd4dd2985f0a5f53681d83c523a2bf
skeleton1231 <814010079@qq.com>
2023-01-20 22:42:15 +0800

    feat:cache secret|Policies

--- internal/apiserver/controller/v1/cache/cache.go
+++ internal/apiserver/controller/v1/cache/cache.go
@@ -6,10 +6,17 @@
 package cache
 
 import (
+	"context"
 	"fmt"
 	"sync"
 
+	"github.com/marmotedu/errors"
+	"github.com/marmotedu/iam/pkg/log"
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/store"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/code"
+
+	pb "github.com/marmotedu/api/proto/apiserver/v1"
+	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
 )
 
 // Cache defines a cache service used to list all secrets and policies.
@@ -35,4 +42,65 @@ func GetCacheInsOr(store store.Factory) (*Cache, error) {
 	}
 
 	return cacheServer, nil
+}
+
+// ListSecrets returns all secrets.
+func (c *Cache) ListSecrets(ctx context.Context, r *pb.ListSecretsRequest) (*pb.ListSecretsResponse, error) {
+	log.L(ctx).Info("list secrets function called.")
+	opts := metav1.ListOptions{
+		Offset: r.Offset,
+		Limit:  r.Limit,
+	}
+
+	secrets, err := c.store.Secrets().List(ctx, "", opts)
+	if err != nil {
+		return nil, errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
+	items := make([]*pb.SecretInfo, 0)
+	for _, secret := range secrets.Items {
+		items = append(items, &pb.SecretInfo{
+			SecretId:    secret.SecretID,
+			Username:    secret.Username,
+			SecretKey:   secret.SecretKey,
+			Expires:     secret.Expires,
+			Description: secret.Description,
+			CreatedAt:   secret.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt:   secret.UpdatedAt.Format("2006-01-02 15:04:05"),
+		})
+	}
+
+	return &pb.ListSecretsResponse{
+		TotalCount: secrets.TotalCount,
+		Items:      items,
+	}, nil
+}
+
+// ListPolicies returns all policies.
+func (c *Cache) ListPolicies(ctx context.Context, r *pb.ListPoliciesRequest) (*pb.ListPoliciesResponse, error) {
+	log.L(ctx).Info("list policies function called.")
+	opts := metav1.ListOptions{
+		Offset: r.Offset,
+		Limit:  r.Limit,
+	}
+
+	policies, err := c.store.Policies().List(ctx, "", opts)
+	if err != nil {
+		return nil, errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
+	items := make([]*pb.PolicyInfo, 0)
+	for _, pol := range policies.Items {
+		items = append(items, &pb.PolicyInfo{
+			Name:         pol.Name,
+			Username:     pol.Username,
+			PolicyShadow: pol.PolicyShadow,
+			CreatedAt:    pol.CreatedAt.Format("2006-01-02 15:04:05"),
+		})
+	}
+
+	return &pb.ListPoliciesResponse{
+		TotalCount: policies.TotalCount,
+		Items:      items,
+	}, nil
 }
