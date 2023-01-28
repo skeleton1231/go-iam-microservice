06c0d949db1ecfa2fe97973f9fc0be63fdcebd23
skeleton1231 <814010079@qq.com>
2023-01-28 22:03:09 +0800

    feat:user get|list|change_password|update

---
+++ internal/apiserver/controller/v1/user/list.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/marmotedu/component-base/pkg/core"
+	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
+	"github.com/marmotedu/errors"
+	"github.com/marmotedu/iam/pkg/log"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/code"
+)
+
+// List list the users in the storage.
+// Only administrator can call this function.
+func (u *UserController) List(c *gin.Context) {
+	log.L(c).Info("list user function called.")
+
+	var r metav1.ListOptions
+	if err := c.ShouldBindQuery(&r); err != nil {
+		core.WriteResponse(c, errors.WithCode(code.ErrBind, err.Error()), nil)
+
+		return
+	}
+
+	users, err := u.srv.Users().List(c, r)
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+
+		return
+	}
+
+	core.WriteResponse(c, nil, users)
+}
