e03d30de7f76752156b91d23fd7d1e0e5f39f485
skeleton1231 <814010079@qq.com>
2023-01-28 21:50:42 +0800

    feat:user create

---
+++ internal/apiserver/controller/v1/user/create.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	v1 "github.com/marmotedu/api/apiserver/v1"
+	"github.com/marmotedu/component-base/pkg/auth"
+	"github.com/marmotedu/component-base/pkg/core"
+	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
+	"github.com/marmotedu/errors"
+	"github.com/marmotedu/iam/pkg/log"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/code"
+)
+
+func (u *UserController) Create(c *gin.Context) {
+	log.L(c).Info("user create function called.")
+
+	var r v1.User
+
+	if err := c.ShouldBindJSON(&r); err != nil {
+		core.WriteResponse(c, errors.WithCode(code.ErrBind, err.Error()), nil)
+
+		return
+	}
+
+	if errs := r.Validate(); len(errs) != 0 {
+		core.WriteResponse(c, errors.WithCode(code.ErrValidation, errs.ToAggregate().Error()), nil)
+
+		return
+	}
+
+	r.Password, _ = auth.Encrypt(r.Password)
+	r.Status = 1
+	r.LoginedAt = time.Now()
+
+	// Insert the user to the storage.
+	if err := u.srv.Users().Create(c, &r, metav1.CreateOptions{}); err != nil {
+		core.WriteResponse(c, err, nil)
+
+		return
+	}
+
+	core.WriteResponse(c, nil, r)
+
+}
