4331f2f637651cf84ddd18533de652f30628fe1c
skeleton1231 <814010079@qq.com>
2023-01-30 18:50:14 +0800

    feat:http basic authentication

--- internal/apiserver/auth.go
+++ internal/apiserver/auth.go
@@ -4,6 +4,17 @@
 
 package apiserver
 
+import (
+	"context"
+	"time"
+
+	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
+
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/store"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/middleware"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/middleware/auth"
+)
+
 const (
 	// APIServerAudience defines the value of jwt audience field.
 	APIServerAudience = "iam.api.marmotedu.com"
@@ -15,4 +26,25 @@ const (
 type loginInfo struct {
 	Username string `form:"username" json:"username" binding:"required,username"`
 	Password string `form:"password" json:"password" binding:"required,password"`
+}
+
+func newBasicAuth() middleware.AuthStrategy {
+
+	return auth.NewBasicStrategy(func(username string, password string) bool {
+		// fetch user from database
+		user, err := store.Client().Users().Get(context.TODO(), username, metav1.GetOptions{})
+		if err != nil {
+			return false
+		}
+
+		// Compare the login password with the user password.
+		if err := user.Compare(password); err != nil {
+			return false
+		}
+
+		user.LoginedAt = time.Now()
+		_ = store.Client().Users().Update(context.TODO(), user, metav1.UpdateOptions{})
+
+		return true
+	})
 }
