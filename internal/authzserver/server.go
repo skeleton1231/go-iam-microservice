fc542271c0973ab1588d8c40df3872c817bfd512
skeleton1231 <814010079@qq.com>
2023-02-01 16:07:38 +0800

    feat:server | route | config ...

--- internal/authzserver/server.go
+++ internal/authzserver/server.go
@@ -3,8 +3,13 @@ package authzserver
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/pkg/shutdown"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/pkg/shutdown/shutdownmanagers/posixsignal"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/pkg/storage"
 
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/config"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/controller/v1/cache"
 	genericoptions "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/options"
 	genericapiserver "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/server"
 )
@@ -27,3 +32,100 @@ type preparedAuthzServer struct {
 }
 
 // func createAuthzServer(cfg *config.Config) (*authzServer, error) {.
+func createAuthzServer(cfg *config.Config) (*authzServer, error) {
+	gs := shutdown.New()
+	gs.AddShutdownManager(posixsignal.NewPosixSignalManager())
+
+	genericConfig, err := buildGenericConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	genericServer, err := genericConfig.Complete().New()
+	if err != nil {
+		return nil, err
+	}
+
+	server := &authzServer{
+		gs:               gs,
+		redisOptions:     cfg.RedisOptions,
+		analyticsOptions: cfg.AnalyticsOptions,
+		rpcServer:        cfg.RPCServer,
+		clientCA:         cfg.ClientCA,
+		genericAPIServer: genericServer,
+	}
+
+	return server, nil
+}
+
+func (s *authzServer) PrepareRun() preparedAuthzServer {
+	_ = s.initialize()
+
+	initRouter(s.genericAPIServer.Engine)
+
+	return preparedAuthzServer{s}
+}
+
+func (s *authzServer) initialize() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	s.redisCancelFunc = cancel
+
+	// keep redis connected
+	go storage.ConnectToRedis(ctx, s.buildStorageConfig())
+
+	// cron to reload all secrets and policies from iam-apiserver
+	cacheIns, err := cache.GetCacheInsOr(apiserver.GetAPIServerFactoryOrDie(s.rpcServer, s.clientCA))
+	if err != nil {
+		return errors.Wrap(err, "get cache instance failed")
+	}
+
+	load.NewLoader(ctx, cacheIns).Start()
+
+	// start analytics service
+	if s.analyticsOptions.Enable {
+		analyticsStore := storage.RedisCluster{KeyPrefix: RedisKeyPrefix}
+		analyticsIns := analytics.NewAnalytics(s.analyticsOptions, &analyticsStore)
+		analyticsIns.Start()
+	}
+
+	return nil
+}
+
+func buildGenericConfig(cfg *config.Config) (genericConfig *genericapiserver.Config, lastErr error) {
+	genericConfig = genericapiserver.NewConfig()
+	if lastErr = cfg.GenericServerRunOptions.ApplyTo(genericConfig); lastErr != nil {
+		return
+	}
+
+	if lastErr = cfg.FeatureOptions.ApplyTo(genericConfig); lastErr != nil {
+		return
+	}
+
+	if lastErr = cfg.SecureServing.ApplyTo(genericConfig); lastErr != nil {
+		return
+	}
+
+	if lastErr = cfg.InsecureServing.ApplyTo(genericConfig); lastErr != nil {
+		return
+	}
+
+	return
+}
+
+func (s *authzServer) buildStorageConfig() *storage.Config {
+	return &storage.Config{
+		Host:                  s.redisOptions.Host,
+		Port:                  s.redisOptions.Port,
+		Addrs:                 s.redisOptions.Addrs,
+		MasterName:            s.redisOptions.MasterName,
+		Username:              s.redisOptions.Username,
+		Password:              s.redisOptions.Password,
+		Database:              s.redisOptions.Database,
+		MaxIdle:               s.redisOptions.MaxIdle,
+		MaxActive:             s.redisOptions.MaxActive,
+		Timeout:               s.redisOptions.Timeout,
+		EnableCluster:         s.redisOptions.EnableCluster,
+		UseSSL:                s.redisOptions.UseSSL,
+		SSLInsecureSkipVerify: s.redisOptions.SSLInsecureSkipVerify,
+	}
+}
