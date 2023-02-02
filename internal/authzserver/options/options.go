2ccdc9f72839080031714b0b4665af89a8325e66
skeleton1231 <814010079@qq.com>
2023-02-02 22:23:21 +0800

    feat:authorize controller

--- internal/authzserver/options/options.go
+++ internal/authzserver/options/options.go
@@ -1,1 +1,37 @@
 package options
+
+import (
+	"github.com/marmotedu/log"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/authzserver/analytics"
+	genericoptions "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/options"
+)
+
+// Options runs a authzserver.
+type Options struct {
+	RPCServer               string                                 `json:"rpcserver"      mapstructure:"rpcserver"`
+	ClientCA                string                                 `json:"client-ca-file" mapstructure:"client-ca-file"`
+	GenericServerRunOptions *genericoptions.ServerRunOptions       `json:"server"         mapstructure:"server"`
+	InsecureServing         *genericoptions.InsecureServingOptions `json:"insecure"       mapstructure:"insecure"`
+	SecureServing           *genericoptions.SecureServingOptions   `json:"secure"         mapstructure:"secure"`
+	RedisOptions            *genericoptions.RedisOptions           `json:"redis"          mapstructure:"redis"`
+	FeatureOptions          *genericoptions.FeatureOptions         `json:"feature"        mapstructure:"feature"`
+	Log                     *log.Options                           `json:"log"            mapstructure:"log"`
+	AnalyticsOptions        *analytics.AnalyticsOptions            `json:"analytics"      mapstructure:"analytics"`
+}
+
+// NewOptions creates a new Options object with default parameters.
+func NewOptions() *Options {
+	o := Options{
+		RPCServer:               "127.0.0.1:8081",
+		ClientCA:                "",
+		GenericServerRunOptions: genericoptions.NewServerRunOptions(),
+		InsecureServing:         genericoptions.NewInsecureServingOptions(),
+		SecureServing:           genericoptions.NewSecureServingOptions(),
+		RedisOptions:            genericoptions.NewRedisOptions(),
+		FeatureOptions:          genericoptions.NewFeatureOptions(),
+		Log:                     log.NewOptions(),
+		AnalyticsOptions:        analytics.NewAnalyticsOptions(),
+	}
+
+	return &o
+}
--- internal/authzserver/server.go
+++ internal/authzserver/server.go
@@ -9,8 +9,9 @@ import (
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/pkg/storage"
 
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/config"
-	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/apiserver/controller/v1/cache"
+
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/authzserver/analytics"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/authzserver/load/cache"
 	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/authzserver/store/apiserver"
 	genericoptions "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/options"
 	genericapiserver "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/server"
