272667cd9ce3472a780c4a4ec442d7a95a36f49e
skeleton1231 <814010079@qq.com>
2023-02-03 21:06:38 +0800

    feat:startPubSubLoop|shouldReload|reloadLoop|DoReload

--- internal/authzserver/load/load.go
+++ internal/authzserver/load/load.go
@@ -59,3 +59,87 @@ func startPubSubLoop() {
 		}
 	}
 }
+
+// shouldReload returns true if we should perform any reload. Reloads happens if
+// we have reload callback queued.
+func shouldReload() ([]func(), bool) {
+	requeueLock.Lock()
+	defer requeueLock.Unlock()
+	if len(requeue) == 0 {
+		return nil, false
+	}
+	n := requeue
+	requeue = []func(){}
+
+	return n, true
+}
+
+func (l *Load) reloadLoop(complete ...func()) {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-l.ctx.Done():
+			return
+		// We don't check for reload right away as the gateway peroms this on the
+		// startup sequence. We expect to start checking on the first tick after the
+		// gateway is up and running.
+		case <-ticker.C:
+			cb, ok := shouldReload()
+			if !ok {
+				continue
+			}
+			start := time.Now()
+			l.DoReload()
+			for _, c := range cb {
+				// most of the callbacks are nil, we don't want to execute nil functions to
+				// avoid panics.
+				if c != nil {
+					c()
+				}
+			}
+			if len(complete) != 0 {
+				complete[0]()
+			}
+			log.Infof("reload: cycle completed in %v", time.Since(start))
+		}
+	}
+}
+
+// reloadQueue used to queue a reload. It's not
+// buffered, as reloadQueueLoop should pick these up immediately.
+var reloadQueue = make(chan func())
+
+var requeueLock sync.Mutex
+
+// This is a list of callbacks to execute on the next reload. It is protected by
+// requeueLock for concurrent use.
+var requeue []func()
+
+func (l *Load) reloadQueueLoop(cb ...func()) {
+	for {
+		select {
+		case <-l.ctx.Done():
+			return
+		case fn := <-reloadQueue:
+			requeueLock.Lock()
+			requeue = append(requeue, fn)
+			requeueLock.Unlock()
+			log.Info("Reload queued")
+			if len(cb) != 0 {
+				cb[0]()
+			}
+		}
+	}
+}
+
+// DoReload reload secrets and policies.
+func (l *Load) DoReload() {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	if err := l.loader.Reload(); err != nil {
+		log.Errorf("faild to refresh target storage: %s", err.Error())
+	}
+
+	log.Debug("refresh target storage succ")
+}
