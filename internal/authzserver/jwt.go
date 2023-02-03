4b15bc583b0b964fa56ea19584dc7e6bcf400315
skeleton1231 <814010079@qq.com>
2023-02-04 01:53:57 +0800

    feat:authserver jwt

---
+++ internal/authzserver/jwt.go
@@ -0,0 +1,36 @@
+// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package authzserver
+
+import (
+	"github.com/pkg/errors"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/authzserver/load/cache"
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/middleware"
+)
+
+func newCacheAuth() middleware.AuthStrategy {
+	return auth.NewCacheStrategy(getSecretFunc())
+}
+
+func getSecretFunc() func(string) (auth.Secret, error) {
+	return func(kid string) (auth.Secret, error) {
+		cli, err := cache.GetCacheInsOr(nil)
+		if err != nil || cli == nil {
+			return auth.Secret{}, errors.Wrap(err, "get cache instance failed")
+		}
+
+		secret, err := cli.GetSecret(kid)
+		if err != nil {
+			return auth.Secret{}, err
+		}
+
+		return auth.Secret{
+			Username: secret.Username,
+			ID:       secret.SecretId,
+			Key:      secret.SecretKey,
+			Expires:  secret.Expires,
+		}, nil
+	}
+}
