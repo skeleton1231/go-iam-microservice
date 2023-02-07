5d30b3409b6f6a5b9f035a9d208bee8d7b0732a5
skeleton1231 <814010079@qq.com>
2023-02-07 20:10:52 +0800

    feat:cli config flags

--- pkg/cli/genericclioptions/config_flags.go
+++ pkg/cli/genericclioptions/config_flags.go
@@ -1,1 +1,20 @@
 package genericclioptions
+
+// Defines flag for iamctl.
+const (
+	FlagIAMConfig     = "iamconfig"
+	FlagBearerToken   = "user.token"
+	FlagUsername      = "user.username"
+	FlagPassword      = "user.password"
+	FlagSecretID      = "user.secret-id"
+	FlagSecretKey     = "user.secret-key"
+	FlagCertFile      = "user.client-certificate"
+	FlagKeyFile       = "user.client-key"
+	FlagTLSServerName = "server.tls-server-name"
+	FlagInsecure      = "server.insecure-skip-tls-verify"
+	FlagCAFile        = "server.certificate-authority"
+	FlagAPIServer     = "server.address"
+	FlagTimeout       = "server.timeout"
+	FlagMaxRetries    = "server.max-retries"
+	FlagRetryInterval = "server.retry-interval"
+)
