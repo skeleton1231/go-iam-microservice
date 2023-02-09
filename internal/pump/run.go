18b7cae7995badec270284d8ed71bac81fcb7d1f
skeleton1231 <814010079@qq.com>
2023-02-09 23:33:33 +0800

    feat:pump run

--- internal/pump/run.go
+++ internal/pump/run.go
@@ -1,1 +1,23 @@
+// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package pump
+
+import (
+	genericapiserver "github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pkg/server"
+
+	"github.com/skeleton1231/go-gin-restful-api-boilerplate/internal/pump/config"
+)
+
+// Run runs the specified pump server. This should never exit.
+func Run(cfg *config.Config, stopCh <-chan struct{}) error {
+	go genericapiserver.ServeHealthCheck(cfg.HealthCheckPath, cfg.HealthCheckAddress)
+
+	server, err := createPumpServer(cfg)
+	if err != nil {
+		return err
+	}
+
+	return server.PrepareRun().Run(stopCh)
+}
