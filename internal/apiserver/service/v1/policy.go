1812c14c9ca5bd368c143edb219b312a78704180
skeleton1231 <814010079@qq.com>
2023-08-17 14:22:30 +0800

    fix:policy udpate|delete

--- internal/apiserver/service/v1/policy.go
+++ internal/apiserver/service/v1/policy.go
@@ -5,7 +5,9 @@ import (
 
 	v1 "github.com/marmotedu/api/apiserver/v1"
 	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
+	"github.com/marmotedu/errors"
 	"github.com/skeleton1231/go-iam-ecommerce-microservice/internal/apiserver/store"
+	"github.com/skeleton1231/go-iam-ecommerce-microservice/internal/pkg/code"
 )
 
 // PolicySrv defines functions used to handle policy request.
@@ -29,14 +31,27 @@ func newPolicies(srv *service) *policyService {
 }
 
 func (s *policyService) Create(ctx context.Context, policy *v1.Policy, opts metav1.CreateOptions) error {
+	if err := s.store.Policies().Create(ctx, policy, opts); err != nil {
+		return errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
 	return nil
 }
 
 func (s *policyService) Update(ctx context.Context, policy *v1.Policy, opts metav1.UpdateOptions) error {
+	// Save changed fields.
+	if err := s.store.Policies().Update(ctx, policy, opts); err != nil {
+		return errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
 	return nil
 }
 
 func (s *policyService) Delete(ctx context.Context, username, name string, opts metav1.DeleteOptions) error {
+	if err := s.store.Policies().Delete(ctx, username, name, opts); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -46,14 +61,27 @@ func (s *policyService) DeleteCollection(
 	names []string,
 	opts metav1.DeleteOptions,
 ) error {
+	if err := s.store.Policies().DeleteCollection(ctx, username, names, opts); err != nil {
+		return errors.WithCode(code.ErrDatabase, err.Error())
+	}
 
 	return nil
 }
 
 func (s *policyService) Get(ctx context.Context, username, name string, opts metav1.GetOptions) (*v1.Policy, error) {
-	return nil, nil
+	policy, err := s.store.Policies().Get(ctx, username, name, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return policy, nil
 }
 
 func (s *policyService) List(ctx context.Context, username string, opts metav1.ListOptions) (*v1.PolicyList, error) {
-	return nil, nil
+	policies, err := s.store.Policies().List(ctx, username, opts)
+	if err != nil {
+		return nil, errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
+	return policies, nil
 }
