2bcbc77adcce0f08dd8bcf566a57f8542dd4db8a
skeleton1231 <814010079@qq.com>
2023-08-29 12:55:05 +0800

    feat:redis methods

--- pkg/storage/redis_clusterV2.go
+++ pkg/storage/redis_clusterV2.go
@@ -339,3 +339,306 @@ func (r *RedisClusterV2) GetMultiKey(keys []string) ([]string, error) {
 	}
 	return nil, ErrKeyNotFound
 }
+
+// GetKeyTTL return ttl of the given key.
+func (r *RedisClusterV2) GetKeyTTL(keyName string) (ttl int64, err error) {
+	if err = r.up(); err != nil {
+		return 0, err
+	}
+	duration, err := r.singleton().TTL(r.Ctx, r.fixKey(keyName)).Result()
+
+	return int64(duration.Seconds()), err
+}
+
+// GetRawKey return the value of the given key.
+func (r *RedisClusterV2) GetRawKey(keyName string) (string, error) {
+	if err := r.up(); err != nil {
+		return "", err
+	}
+	value, err := r.singleton().Get(r.Ctx, keyName).Result()
+	if err != nil {
+		log.Debugf("Error trying to get value: %s", err.Error())
+
+		return "", ErrKeyNotFound
+	}
+
+	return value, nil
+}
+
+// GetExp return the expiry of the given key.
+func (r *RedisClusterV2) GetExp(keyName string) (int64, error) {
+	log.Debugf("Getting exp for key: %s", r.fixKey(keyName))
+	if err := r.up(); err != nil {
+		return 0, err
+	}
+
+	value, err := r.singleton().TTL(r.Ctx, r.fixKey(keyName)).Result()
+	if err != nil {
+		log.Errorf("Error trying to get TTL: ", err.Error())
+
+		return 0, ErrKeyNotFound
+	}
+
+	return int64(value.Seconds()), nil
+}
+
+// SetExp set expiry of the given key.
+func (r *RedisClusterV2) SetExp(keyName string, timeout time.Duration) error {
+	if err := r.up(); err != nil {
+		return err
+	}
+	err := r.singleton().Expire(r.Ctx, r.fixKey(keyName), timeout).Err()
+	if err != nil {
+		log.Errorf("Could not EXPIRE key: %s", err.Error())
+	}
+
+	return err
+}
+
+// SetKey will create (or update) a key value in the store.
+func (r *RedisClusterV2) SetKey(keyName, session string, timeout time.Duration) error {
+	log.Debugf("[STORE] SET Raw key is: %s", keyName)
+	log.Debugf("[STORE] Setting key: %s", r.fixKey(keyName))
+
+	if err := r.up(); err != nil {
+		return err
+	}
+	err := r.singleton().Set(r.Ctx, r.fixKey(keyName), session, timeout).Err()
+	if err != nil {
+		log.Errorf("Error trying to set value: %s", err.Error())
+
+		return err
+	}
+
+	return nil
+}
+
+// SetRawKey set the value of the given key.
+func (r *RedisClusterV2) SetRawKey(keyName, session string, timeout time.Duration) error {
+	if err := r.up(); err != nil {
+		return err
+	}
+	err := r.singleton().Set(r.Ctx, keyName, session, timeout).Err()
+	if err != nil {
+		log.Errorf("Error trying to set value: %s", err.Error())
+
+		return err
+	}
+
+	return nil
+}
+
+// Decrement will decrement a key in redis.
+func (r *RedisClusterV2) Decrement(keyName string) {
+	keyName = r.fixKey(keyName)
+	log.Debugf("Decrementing key: %s", keyName)
+	if err := r.up(); err != nil {
+		return
+	}
+	err := r.singleton().Decr(r.Ctx, keyName).Err()
+	if err != nil {
+		log.Errorf("Error trying to decrement value: %s", err.Error())
+	}
+}
+
+// IncrememntWithExpire will increment a key in redis.
+func (r *RedisClusterV2) IncrememntWithExpire(keyName string, expire int64) int64 {
+	log.Debugf("Incrementing raw key: %s", keyName)
+	if err := r.up(); err != nil {
+		return 0
+	}
+	// This function uses a raw key, so we shouldn't call fixKey
+	fixedKey := keyName
+	val, err := r.singleton().Incr(r.Ctx, fixedKey).Result()
+
+	if err != nil {
+		log.Errorf("Error trying to increment value: %s", err.Error())
+	} else {
+		log.Debugf("Incremented key: %s, val is: %d", fixedKey, val)
+	}
+
+	if val == 1 && expire > 0 {
+		log.Debug("--> Setting Expire")
+		r.singleton().Expire(r.Ctx, fixedKey, time.Duration(expire)*time.Second)
+	}
+
+	return val
+}
+
+// GetKeys will return all keys according to the filter (filter is a prefix - e.g. tyk.keys.*).
+func (r *RedisClusterV2) GetKeys(filter string) []string {
+	if err := r.up(); err != nil {
+		return nil
+	}
+	client := r.singleton()
+
+	filterHash := ""
+	if filter != "" {
+		filterHash = r.hashKey(filter)
+	}
+	searchStr := r.KeyPrefix + filterHash + "*"
+	log.Debugf("[STORE] Getting list by: %s", searchStr)
+
+	fnFetchKeys := func(client *redis.Client) ([]string, error) {
+		values := make([]string, 0)
+
+		iter := client.Scan(r.Ctx, 0, searchStr, 0).Iterator()
+		for iter.Next(r.Ctx) {
+			values = append(values, iter.Val())
+		}
+
+		if err := iter.Err(); err != nil {
+			return nil, err
+		}
+
+		return values, nil
+	}
+
+	var err error
+	var values []string
+	sessions := make([]string, 0)
+
+	switch v := client.(type) {
+	case *redis.ClusterClient:
+		ch := make(chan []string)
+
+		go func() {
+			err = v.ForEachMaster(r.Ctx, func(ctx context.Context, client *redis.Client) error {
+				values, err = fnFetchKeys(client)
+				if err != nil {
+					return err
+				}
+
+				ch <- values
+
+				return nil
+			})
+			close(ch)
+		}()
+
+		for res := range ch {
+			sessions = append(sessions, res...)
+		}
+	case *redis.Client:
+		sessions, err = fnFetchKeys(v)
+	}
+
+	if err != nil {
+		log.Errorf("Error while fetching keys: %s", err)
+
+		return nil
+	}
+
+	for i, v := range sessions {
+		sessions[i] = r.cleanKey(v)
+	}
+
+	return sessions
+}
+
+// GetKeysAndValuesWithFilter will return all keys and their values with a filter.
+func (r *RedisClusterV2) GetKeysAndValuesWithFilter(filter string) map[string]string {
+	if err := r.up(); err != nil {
+		return nil
+	}
+	keys := r.GetKeys(filter)
+	if keys == nil {
+		log.Error("Error trying to get filtered client keys")
+
+		return nil
+	}
+
+	if len(keys) == 0 {
+		return nil
+	}
+
+	for i, v := range keys {
+		keys[i] = r.KeyPrefix + v
+	}
+
+	client := r.singleton()
+	values := make([]string, 0)
+
+	switch v := client.(type) {
+	case *redis.ClusterClient:
+		{
+			getCmds := make([]*redis.StringCmd, 0)
+			pipe := v.Pipeline()
+			for _, key := range keys {
+				getCmds = append(getCmds, pipe.Get(r.Ctx, key))
+			}
+			_, err := pipe.Exec(r.Ctx)
+			if err != nil && !errors.Is(err, redis.Nil) {
+				log.Errorf("Error trying to get client keys: %s", err.Error())
+
+				return nil
+			}
+
+			for _, cmd := range getCmds {
+				values = append(values, cmd.Val())
+			}
+		}
+	case *redis.Client:
+		{
+			result, err := v.MGet(r.Ctx, keys...).Result()
+			if err != nil {
+				log.Errorf("Error trying to get client keys: %s", err.Error())
+
+				return nil
+			}
+
+			for _, val := range result {
+				strVal := fmt.Sprint(val)
+				if strVal == "<nil>" {
+					strVal = ""
+				}
+				values = append(values, strVal)
+			}
+		}
+	}
+
+	m := make(map[string]string)
+	for i, v := range keys {
+		m[r.cleanKey(v)] = values[i]
+	}
+
+	return m
+}
+
+// GetKeysAndValues will return all keys and their values - not to be used lightly.
+func (r *RedisClusterV2) GetKeysAndValues() map[string]string {
+	return r.GetKeysAndValuesWithFilter("")
+}
+
+// DeleteKey will remove a key from the database.
+func (r *RedisClusterV2) DeleteKey(keyName string) bool {
+	if err := r.up(); err != nil {
+		// log.Debug(err)
+		return false
+	}
+	log.Debugf("DEL Key was: %s", keyName)
+	log.Debugf("DEL Key became: %s", r.fixKey(keyName))
+	n, err := r.singleton().Del(r.Ctx, r.fixKey(keyName)).Result()
+	if err != nil {
+		log.Errorf("Error trying to delete key: %s", err.Error())
+	}
+
+	return n > 0
+}
+
+// DeleteAllKeys will remove all keys from the database.
+func (r *RedisClusterV2) DeleteAllKeys() bool {
+	if err := r.up(); err != nil {
+		return false
+	}
+	n, err := r.singleton().FlushAll(r.Ctx).Result()
+	if err != nil {
+		log.Errorf("Error trying to delete keys: %s", err.Error())
+	}
+
+	if n == "OK" {
+		return true
+	}
+
+	return false
+}
